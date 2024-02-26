d0f8ae1fffdb5d46a1e9edd4387d19fdb59f5052
Qian Zhang <zhangqian_98sh@outlook.com>
2024-02-25 20:31:59 -0500

    Add nvim-tree.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+      vim.keymap.set('n', '<leader>f', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+    end,
+  }
+}
